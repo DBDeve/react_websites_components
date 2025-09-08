178ba05318ce9f4b99849977996eb1418792fb10
dario bertonati <dario.bertonatilavoro@gmail.com>
2025-09-08 17:58:17 +0200

    update pageContent component

--- src/PageContent/PageContent.tsx
+++ src/PageContent/PageContent.tsx
@@ -1,6 +1,10 @@
 import React, {ReactNode,ReactElement} from 'react';
 import styles from './PageContent.module.css';
 
+// ADD THIS TO CUSTOM BUTTON
+//button?:{color:string, text:{color:string, content:string}, padding:string, border:{borderColor:string, borderRadius:string, style:string}, margin:string}
+
+
 
 type Page = {
     h1:{size?:string,family?:string,content:string},
@@ -33,16 +37,16 @@ type video={ type:'video', src:string}
 type HeroSection={
     mediaType:video | image,
     h2?:{size?:string,family?:string,content:string}
-    button?:{}
+    button?:{padding?:string, text:{color:string, content:string}}
 }
 export const HeroSection:React.FC<HeroSection> = ({mediaType,h2,button}) => {
     return(
         <section id='hero_section' className={styles.HeroSection}>
             {mediaType.type === 'video' && <video></video>}
-            {mediaType.type === 'image' && (<img src="immagine.jpg" alt="immagine hero section" loading="lazy"/>)}
+            {mediaType.type === 'image' && (<img src={`${mediaType.src}`} className={`${styles.hero_img}`}alt="immagine hero section" loading="lazy"/>)}
             <div className={styles.HeroContent}>
-                {h2 && <h2 style={{ '--h2hero-size': h2.size, '--h2hero-family':h2.family } as React.CSSProperties} className={`${styles.h2}`}> {h2.content} </h2>}
-                {button && <button> </button>}
+                {h2 && <h2 style={{ '--h2hero-size': h2.size, '--h2hero-family':h2.family } as React.CSSProperties} className={`${styles.h2HeroSection}`}> {h2.content} </h2>}
+                {button && <button style={{ '--buttonHero-padding': button.padding } as React.CSSProperties} className={`${styles.buttonHeroSection}`}> vai al sito </button>}
             </div>
         </section>
     )
