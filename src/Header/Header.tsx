4c4342041a7995e1a7f0c7862fbfeab7c23a0bd1
dario bertonati <dario.bertonatilavoro@gmail.com>
2025-11-23 18:42:32 +0100

    fixed header error

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -391,6 +391,15 @@ export const Header: React.FC<HeaderProps> = ({ children, backGroundColor, paddi
       }
     }, []);
   }
+
+  useEffect(() => {
+    const header = document.querySelector('header');
+    const mobileMenu = document.getElementById('mobile_menu');
+    if(mobileMenu && header){
+      const headerHeight = header.clientHeight;
+      mobileMenu.style.setProperty('--mobile-menu-top', `${headerHeight}px`);
+    }
+  },[]);
   
   return (
     <header role="banner" style={{ '--bg-color': backGroundColor,'--hover-color': hoverColor,'--padding':padding } as React.CSSProperties} className={`${styles.header} ${positionClass}` }>
